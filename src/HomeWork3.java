7f5153714efff9fd66d245d238ae465d8c3519fa
VeronicaZhigalova <145050893+VeronicaZhigalova@users.noreply.github.com>
2023-09-23 18:41:14 +0300

    Update HomeWork3.java
    Changes

--- src/HomeWork3.java
+++ src/HomeWork3.java
@@ -1,49 +1,37 @@
 public class HomeWork3 {
+    public static int calculateAbsoluteDifference(int a, int b) {
+       return (-1) * (a - b);
+    }
+
+
+
+
+    public char getFirstChar(String string) {
+        return string.charAt(0);
+    }
+
+    public char getLastChar(String string) {
+        return string.charAt(string.length() - 1);
+    }
+
+
+    public String swapFirstAndLastCharacters(String string) {
+        return getLastChar(string) + string.substring(1, string.length() - 1) + getFirstChar(string);
+
+    }
+
     public static void main(String[] args) {
-        int number1 = 5;
-        int number2 = 9;
-        int result = (number1 - number2);
-        if (result > 0) {
-            System.out.print(result);
-        } else {
-            System.out.println(-1 * result);
-
-        }
-
-    }
-
-}
-class Letter2 {
-    public static void main(String[] args) {
-
-        String text = "Python";
-        char a = text.charAt(0);
-        System.out.println(a);
-        char b = text.charAt(text.length() - 1);
-        System.out.println(b);
-
+        int calculateAbsoluteDifference = calculateAbsoluteDifference(-8, 7);
+        HomeWork4 homework = new HomeWork4();
+        char firstChar = homework.getFirstChar("Python");
+        char lastChar = homework.getLastChar("Python");
+        System.out.println(firstChar);
+        System.out.println(lastChar);
+        String swappedString = homework.swapFirstAndLastCharacters("Python");
+        System.out.println(swappedString);
+        System.out.println(calculateAbsoluteDifference);
     }
 
 }
 
-
-class Word2 {
-    public static String swapFirstAndLast(String str) {
-        if (str.length() < 2)
-            return str;
-
-        char[] ch = str.toCharArray();
-        char temp = ch[0];
-        ch[0] = ch[ch.length - 1];
-        ch[ch.length - 1] = temp;
-
-        return String.valueOf(ch);
-    }
-
-    public static void main(String args[]) {
-
-        String str = "Python";
-        System.out.println(swapFirstAndLast(str));
-    }
-}
 
